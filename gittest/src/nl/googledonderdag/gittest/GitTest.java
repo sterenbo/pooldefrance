e1c2197ebd9b3a8218c05d15dc93074cf749fccb
sterenbo <sterenbo@gmail.com>
2012-12-20 16:21:49 +0100

    Update gittest/src/nl/googledonderdag/gittest/GitTest.java

--- gittest/src/nl/googledonderdag/gittest/GitTest.java
+++ gittest/src/nl/googledonderdag/gittest/GitTest.java
@@ -2,7 +2,7 @@ package nl.googledonderdag.gittest;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is geek stuff");
+    System.out.println("Git is geek stuff!!!");
     // en dat is geen leugen
   }
 } 
